253aad8addf068aeab281b8716f27559c61760ce
Swarnavo Pramanik <105142693+swarnavopramanik@users.noreply.github.com>
2023-07-04 01:02:44 +0530

    Create Remove Linked List Elements.java

---
+++ Remove Linked List Elements.java
@@ -0,0 +1,22 @@
+203. Remove Linked List Elements
+
+
+  class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+        while(head != null && head.val == val) {
+            head = head.next;
+        }
+        if(head == null) {
+            return head;
+        }
+        ListNode temp = head;
+        while(temp.next != null) {
+            if(temp.next.val == val) {
+                temp.next = temp.next.next;
+            } else {
+                temp = temp.next;
+            }
+        }
+        return head;
+    }
+}
