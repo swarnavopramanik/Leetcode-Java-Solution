f0efa1ccf5eb12a35a71d8381f77d536a2d9483a
Swarnavo Pramanik <105142693+swarnavopramanik@users.noreply.github.com>
2023-07-22 18:51:58 +0530

    Create Dungeon Game.java

---
+++ Dungeon Game.java
@@ -0,0 +1,27 @@
+174. Dungeon Game
+Hard
+
+  class Solution {
+    public int calculateMinimumHP(int[][] dungeon) {
+        if (dungeon == null || dungeon.length == 0 || dungeon[0].length == 0)
+            return 0;
+        HashMap<String, Integer> hm = new HashMap<>();
+        return rec(0, 0, dungeon, hm);
+    }
+    
+    private int rec(int i, int j, int[][] dungeon, HashMap<String, Integer> hm) {
+        if(i >= dungeon.length || j >= dungeon[0].length)
+            return Integer.MAX_VALUE;
+        String key = i + "Swarnavo"  + j;
+        if(hm.containsKey(key))
+             return hm.get(key);
+        int next = Math.min(rec(i+1, j, dungeon, hm), rec(i, j+1, dungeon, hm));
+    
+        if(next == Integer.MAX_VALUE)
+           next  = 1;
+        
+        int res = Math.max(next - dungeon[i][j], 1);
+        hm.put(key, res);
+        return res;
+    }
+}
